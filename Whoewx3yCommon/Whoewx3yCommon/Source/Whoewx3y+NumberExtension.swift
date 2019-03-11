365f3eb2f93668099d95c410756db804a8752bc8
nutdanai <>
2019-03-11 10:42:34 +0700

    create lib

---
+++ Whoewx3yCommon/Whoewx3yCommon/Source/Whoewx3y+NumberExtension.swift
@@ -0,0 +1,43 @@
+//
+//  Whoewx3y+NumberExtension.swift
+//  Whoewx3yCommon
+//
+//  Created by nutdanai on 11/3/2562 BE.
+//  Copyright © 2562 nutdanai. All rights reserved.
+//
+
+import UIKit
+
+public extension Int {
+    public func abbreviateNumber() -> String{
+        if self < 1000 {
+            return "\(self)"
+        }
+        
+        if self < 1000000 {
+            var n = Double(self);
+            n = Double( floor(n/100)/10 )
+            return "\(n.description)k"
+        }
+        
+        var n = Double(self)
+        n = Double( floor(n/100000)/10 )
+        
+        return "\(n.description)M"
+    }
+    
+    public func zeroToDat() -> String {
+        if self > 0 {
+            return "\(self)"
+        }
+        return "-"
+    }
+}
+
+public extension Double {
+    
+}
+
+public extension Float {
+    
+}
---
+++ Whoewx3yCommon/whoewx3yExtension.podspec
@@ -0,0 +1,27 @@
+
+
+Pod::Spec.new do |s|
+
+
+  s.name         = "whoewx3yExtension"
+  s.version      = "0.0.1"
+  s.summary      = "A short description of whoewx3yExtension."
+  s.license      = "MIT"
+
+  s.author             = { "nutdanai" => "mellonb77@gmail.com" }
+
+  s.ios.deployment_target = "8.0"
+
+
+  s.framework  = "UIKit"
+  s.requires_arc = true
+
+  s.subspec "All" do |ss|
+   ss.ios.dependency 'whoewx3yCommon/whoewx3yCommon'
+  end
+
+  s.subspec "whoewx3yCommon" do |ss|
+   ss.ios.dependency 'whoewx3yCommon/Source/**'
+  end
+
+end
