f0fac772a4a228d24dfcac3c60c9300da65f3c5e
nutdanai <>
2019-03-27 15:30:28 +0700

    umm 0.09

--- Whoewx3yCommon/Whoewx3yCommon/Main/ViewController.swift
+++ Whoewx3yCommon/Whoewx3yCommon/Main/ViewController.swift
@@ -15,11 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet var headerView: Whoewx3yViewMask!
     @IBOutlet var subView: Whoewx3yViewMask!
     
-    let function:[[String]] = [
-        ["Number","String"]
-        ,["Some function"]
-    ]
-    let header:[String] = ["Extension","Function"]
+    lazy var extentionVC: ExtentionDetailViewController = {
+        let dtVC = ExtentionDetailViewController(nibName: "ExtentionDetailViewController", bundle: nil)
+        return dtVC
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,17 +54,27 @@ extension ViewController {
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return function.count
+        return MainConstaint.mainData.menu.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return function[section].count
+        return MainConstaint.mainData.menu[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = function[indexPath.section][indexPath.row]
+        cell.textLabel?.text = MainConstaint.mainData.menu[indexPath.section][indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+//            Extension
+            extentionVC.vcTag = MainConstaint.mainData.menu[0][indexPath.row]
+            self.pushVC(vc: extentionVC)
+        } else if indexPath.section == 1 {
+            
+        }
     }
     
 //    header
@@ -73,7 +82,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let view = UIView()
         view.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
         let lbHeader = UILabel()
-        lbHeader.text = "*\(header[section])*"
+        lbHeader.text = "*\(MainConstaint.mainData.menuHeader[section])*"
         lbHeader.textColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
         lbHeader.frame = CGRect(x: 10,y: 0,width: 100,height: 50)
         view.addSubview(lbHeader)
